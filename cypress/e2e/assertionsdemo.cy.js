dcb828aeb1faefd26990364a0b61f51542a9f1ec
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2024-03-15 17:31:59 +0530

    Learning assertions in cypress

---
+++ cypress/e2e/assertionsdemo.cy.js
@@ -0,0 +1,22 @@
+it('Assertions Demo', () => {
+
+    cy.visit('https://example.cypress.io')
+    // cy.get(':nth-child(4) > .row > .col-xs-12 > .home-list > :nth-child(1) > :nth-child(1)').click()
+    cy.contains('get').click()
+    cy.get('#query-btn')
+        .should('contain','Button')
+        .and('have.class','query-btn')
+        .and('be.visible')
+        .and('be.enabled')
+
+    expect(true).to.be.true
+    // let name = 'abhijeet';
+    // expect(name).to.be.equal('abhijeet')
+
+    // assert.equal(4,5,'NOT EQUAL')
+    assert.equal(4,'4','NOT EQUAL')
+    // assert.strictEqual(4,'4','NOT EQUAL')
+
+    
+    
+})
